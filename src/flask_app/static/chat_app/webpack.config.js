114342c9afe998e672e2c17945400778ad7ec7c7
mingz <305603665@qq.com>
2016-09-19 22:32:36 +0800

    init

---
+++ src/flask_app/static/chat_app/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+
+    entry: [path.resolve(ROOT_PATH, 'app/containers/app.js')],
+
+    output: {
+        path: path.resolve(ROOT_PATH, '../dist'),
+        filename: 'chat.js'
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                loader: "babel",
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
