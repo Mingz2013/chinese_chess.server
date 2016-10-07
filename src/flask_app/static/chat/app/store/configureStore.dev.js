8b32c98db377e0426ce03544de72958beb5c9b44
mingz <305603665@qq.com>
2016-10-07 13:54:49 +0800

    init

--- src/flask_app/static/chat/app/store/configureStore.dev.js
+++ src/flask_app/static/chat/app/store/configureStore.dev.js
@@ -9,7 +9,8 @@ import DevTools from '../containers/DevTools';
 
 const enhancer = compose(
     applyMiddleware(thunk),
-    DevTools.instrument()
+    //DevTools.instrument()
+    window.devToolsExtension ? window.devToolsExtension() : f => f
     //persistState(
     //  window.location.href.match(
     //    /[?&]debug_session=([^&#]+)\b/
