9852a06e8d58058f700f392635d5c0f88e60ce85
mingz <305603665@qq.com>
2016-11-18 20:09:32 +0800

    init

--- apps/website/controllers/views/home_controller.py
+++ apps/website/controllers/views/home_controller.py
@@ -12,21 +12,6 @@ def index():
     return render_template("home/index.html")
 
 
-@api.route('/chat', methods=['GET'])
-def chat():
-    return render_template("chat.html")
-
-
-@api.route('/blog', methods=['GET'])
-def blog():
-    return render_template("blog.html")
-
-
-@api.route('/game', methods=['GET'])
-def game():
-    return render_template("game.html")
-
-
 @api.route('/about', methods=['GET'])
 def about():
     return render_template("home/about.html")
