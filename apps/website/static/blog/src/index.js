e7f26a187e56c078ddd766a6c859a807339db0b2
mingz <305603665@qq.com>
2016-11-21 15:49:20 +0800

    init

--- apps/website/static/blog/src/index.js
+++ apps/website/static/blog/src/index.js
@@ -2,9 +2,15 @@
  * Created by zhaojm on 13/11/2016.
  */
 
-import '../css/style.css'
+//import $ from 'jQuery'
 
-import $ from 'jQuery'
+//import 'bootstrap-sass'
+
+
+var $ = window.$ = window.jQuery = require('jquery'),
+    bootstrap = require('bootstrap-sass');
+
+import '../css/style.css'
 
 //window.$ = $; // TODO 没用??
 
