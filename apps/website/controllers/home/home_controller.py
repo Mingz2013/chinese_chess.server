7aa046fcf8085d70361a67bc14c88eaf77e5af50
mingz <305603665@qq.com>
2016-11-21 15:08:46 +0800

    init

--- apps/website/controllers/home/home_controller.py
+++ apps/website/controllers/home/home_controller.py
@@ -9,17 +9,17 @@ api = Blueprint('home_controller', __name__, url_prefix='')
 
 @api.route('/', methods=['GET'])
 def index():
-    return render_template("home/index.html")
+    return render_template("home/home/index.html")
 
 
 @api.route('/about', methods=['GET'])
 def about():
-    return render_template("home/about.html")
+    return render_template("home/home/about.html")
 
 
 @api.route('/contact', methods=['GET'])
 def contact():
-    return render_template("home/contact.html")
+    return render_template("home/home/contact.html")
 
 # @api.route('/debug', methods=['GET'])
 # def debug():
